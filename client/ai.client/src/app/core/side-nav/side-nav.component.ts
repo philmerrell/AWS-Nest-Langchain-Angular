3c9e329efb494f9b4c630278614bc170f8211d2f
Phil Merrell <philmerrell@boisestate.edu>
2025-03-22 17:23:38 -0600

    Add SideNav component; integrate into app layout and remove unused imports

---
+++ client/ai.client/src/app/core/side-nav/side-nav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss'],
+  standalone: true
+})
+export class SideNavComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  getConversations() {
+
+  }
+
+}
